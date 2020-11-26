8f7d0520c1ac76f645f23b3279cb9298abfd190a
regi-gouale <74569854+regi-gouale@users.noreply.github.com>
2020-11-26 23:17:56 +0100

    Creation of the first sketch Create the fisrt sketch of the application

--- index.js
+++
@@ -1,9 +0,0 @@
-const {ipcRenderer} = require('electron');
-
-$('#changeTitle').on('click', () => {
-    ipcRenderer.send('getNewTitle', '');
-});
-
-ipcRenderer.on('giveNewTitle', (evt, data) => {
-    $('h1').text(data);
-});
--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const {app, BrowserWindow, ipcMain} = require('electron');
 
 function createWindow(){
     let win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
         webPreferences: {
             nodeIntegration: true
         }
     });
-    win.loadFile('index.html');
+    win.loadFile('views/home/home.html');
 
     win.on('closed', () => {
-        win = null
+        win = null;
     });
 
     return win
